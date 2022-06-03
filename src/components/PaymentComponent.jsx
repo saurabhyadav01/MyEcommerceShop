838a8592389083d9f628872ab925cc037c4de910
saurabh yadav <72351102+saurabhyadav01@users.noreply.github.com>
2022-06-03 18:33:40 +0530

    Update PaymentComponent.jsx

--- src/components/PaymentComponent.jsx
+++ src/components/PaymentComponent.jsx
@@ -162,7 +162,7 @@ export default function Payments() {
               sx={{ mt: 3, mb: 2 }}
             >
               Pay
-              {/* <a href="/home/products/cart/payment/success"> Pay</a> */}
+              a href="/home/products/cart/payment/success"> Pay</a> 
             </Button>
           </Box>
         </Box>
